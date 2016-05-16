2d1ebf4349aac54cb2c9e74447fb43e72187aee1
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2016-05-16 09:36:18 +0000

    added stemming support. added advanced work filtering to litteraturbanken web api.
    git-svn-id: https://svn.spraakdata.gu.se/repos/strix/trunk/strix@238814 ef392f78-ee14-0410-84e5-9db46baae5bb

--- mapping_util.py
+++ mapping_util.py
@@ -1,17 +1,35 @@
 from elasticsearch_dsl import analysis, analyzer, tokenizer
 
 
-def annotation_analyzer(annotation_name):
+def annotation_analyzer(annotation_name, isSet=False):
     """
     create an analyzer for a specific annotation found a token s.a. framtid|wid=12|page=3||
     for example passing "wid" as parameter and using anayzer on a field in a type "text "will enable
     searching on text.wid
+    @param isSet: if true, the pattern annotation_name=value gives value as a comma-separated list. 
     """
     filter_name = annotation_name + "_filter"
     analyzer_name = annotation_name + "_analyzer"
+    patterns = []
+    """
+    so this doesn't work. i can't make a regexp that can find 
+    Korp's set attributes (such as lemgram) and yield a capturing group for 
+    each member of the set :( 
+    """
+    if isSet:
+        pass
+        # patterns.append("\\|" + annotation_name + "=(?:(.+?)_)+\\|")
+        # patterns.append("\\|" + annotation_name + "=(.*?)\\|")
+        # patterns.append("\\|" + annotation_name + "=(.*?)_(.*?)\\|")
+        # patterns.append("\\|" + annotation_name + "=(.+?)_?(.+?)?_?(.+?)?\\|")
+    else:
+        pass
+    # because set isn't supported, assume one value only in annotation
+    patterns.append("\\|" + annotation_name + "=(.+?)\\|")
 
-    a_filter = analysis.token_filter(filter_name, 'pattern_capture', preserve_original=False, patterns=["\\|"
-                                     + annotation_name + "=(.+?)\\|"])
+
+
+    a_filter = analysis.token_filter(filter_name, 'pattern_capture', preserve_original=False, patterns=patterns)
     return analysis.analyzer(analyzer_name, tokenizer='whitespace', filter=["lowercase", a_filter])
 
 
@@ -20,7 +38,8 @@ def get_swedish_analyzer():
     uses pattern_capture token filter to change input from framtid|wid=12|page=3|| to token "framtid"
     """
     payload_strip = analysis.token_filter('payload_strip', 'pattern_capture', preserve_original=False, patterns=["^(.*?)\\|.*"])
-    return analyzer('swedish',  tokenizer=tokenizer('whitespace'), filter=['lowercase', payload_strip])
+    stemmer = analysis.token_filter("swedish_stemmer", type="stemmer_override", rules_path="/Users/johan/Dropbox_lb/dev/strix/trunk/analyzers/stems.txt")
+    return analyzer('swedish',  tokenizer=tokenizer('whitespace'), filter=['lowercase', payload_strip, stemmer])
 
 
 def default_annotation_analyzer():
