76744f16b659ce1ad5efe3f2dc4ca997f853a233
mariao <mariao@ef392f78-ee14-0410-84e5-9db46baae5bb>
2017-01-25 12:31:00 +0000

    parameterize port
    git-svn-id: https://svn.spraakdata.gu.se/repos/strix/trunk/strix@249603 ef392f78-ee14-0410-84e5-9db46baae5bb

--- bin/strix-api.py
+++ bin/strix-api.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+import sys
 from strix.api.web import app
 
 from waitress import serve
-serve(app, host='0.0.0.0', port=8081)
+
+try:
+    port = sys.argv[1]
+except IndexError:
+    sys.exit("Usage %s <port>" % sys.argv[0])
+
+
+serve(app, host='0.0.0.0', port=port)
 
 
