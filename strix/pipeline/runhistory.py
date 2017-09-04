d8470d74f7901a912d625c3e5469981cfdbcca1f
Maria Öhrman <maria.ohrman@gu.se>
2017-09-04 09:19:59 +0000

    add svn rev
    git-svn-id: https://svn.spraakdata.gu.se/repos/strix/trunk/strix@260509 ef392f78-ee14-0410-84e5-9db46baae5bb

--- strix/pipeline/runhistory.py
+++ strix/pipeline/runhistory.py
@@ -1,11 +1,26 @@
 import elasticsearch
+import subprocess
 from strix.config import config
 
 es = elasticsearch.Elasticsearch(config.elastic_hosts)
 index_name = ".runhistory"
 
 
+def get_svn_revision():
+    base_dir = config.base_dir
+    try:
+        output = subprocess.check_output(["svn", "info"], cwd=base_dir, stderr=subprocess.STDOUT).decode("UTF-8")
+    except subprocess.CalledProcessError:
+        output = subprocess.check_output(["git", "svn", "info"], cwd=base_dir).decode("UTF-8")
+    except:
+        output = "Revision: N/A"
+    lines = output.split("\n")
+    rev_line = filter(lambda line: line.startswith("Revision"), lines)
+    return list(next(rev_line).split(": "))[1]
+
+
 def put(obj):
+    obj["svn_rev"] = get_svn_revision()
     es.index(index_name, "entry", obj)
 
 
@@ -14,7 +29,33 @@ def create():
         return
     else:
         settings = {
-            "number_of_shards": 1,
-            "number_of_replicas": 1
+            "settings": {
+                "index": {
+                    "number_of_shards": 1,
+                    "number_of_replicas": 1
+                }
+            },
+            "mappings": {
+                "entry": {
+                    "properties": {
+                        "elastic_hosts": {
+                            "properties": {
+                                "host": {
+                                    "type": "keyword"
+                                },
+                                "port": {
+                                    "type": "long"
+                                }
+                            }
+                        },
+                        "index": {
+                            "type": "keyword"
+                        },
+                        "svn_rev": {
+                            "type": "keyword"
+                        }
+                    }
+                }
+            }
         }
         es.indices.create(index_name, body=settings)
