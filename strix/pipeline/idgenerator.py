af1dd2d22eb3b560715363eb4c4b2bb0869d1f18
Maria Öhrman <maria.ohrman@gu.se>
2017-09-06 09:33:13 +0000

    don't print ugly exception and try streaming bulk instead of bulk
    git-svn-id: https://svn.spraakdata.gu.se/repos/strix/trunk/strix@260580 ef392f78-ee14-0410-84e5-9db46baae5bb

--- strix/pipeline/idgenerator.py
+++ strix/pipeline/idgenerator.py
@@ -1,10 +1,11 @@
 import elasticsearch
+import logging
 from elasticsearch_dsl import Index, Mapping
 from strix.config import config
 
 
 es = elasticsearch.Elasticsearch(config.elastic_hosts, timeout=60)
-
+_logger = logging.getLogger(__name__)
 
 def create_sequence_index():
     sequence_index = Index("sequence", using=es)
@@ -30,7 +31,11 @@ def remove_sequence_index():
 
 
 def get_id_sequence(index_name, size):
-    tasks = "".join(['{"index": {"_index": "sequence", "_type": "sequence", "_id": "' + index_name + '", "_source": {}}}\n{}\n' for _ in range(0, size)])
-    result = es.bulk(body=tasks)
-    for item in result['items']:
-        yield item["index"]["_version"]
+    tasks = [{"_index": "sequence", "_type": "sequence", "_id": index_name, "_source": {}, "_op_type": "index"} for _ in range(0, size)]
+    try:
+        for (success, item) in elasticsearch.helpers.streaming_bulk(es, tasks):
+            if success:
+                yield item["index"]["_version"]
+    except elasticsearch.exceptions.SerializationError:
+        raise RuntimeError("Failure getting IDs (got elasticsearch.exceptions.SerializationError), size: " + str(size)) from None
+
