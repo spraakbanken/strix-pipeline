6f22ce3d6716c5d5e1776d49e32df62749e338e2
Maria Öhrman <maria.ohrman@gu.se>
2024-10-22 09:51:26 +0200

    fix: remove exluding from _source to make it possible to use ES update API

--- strixpipeline/createindex.py
+++ strixpipeline/createindex.py
@@ -137,14 +137,12 @@ class CreateIndex:
     def create_text_type(self, index_name):
         m = Mapping()
         m.meta("dynamic", "strict")
-        excludes = ["text", "wid", "sent_vector"]
 
         m.field("text", Text(analyzer=mappingutil.token_analyzer()))
         m.field("wid", Text(analyzer=mappingutil.annotation_analyzer()))
 
         for attr in self.word_attributes:
             annotation_name = attr["name"]
-            excludes.append("pos_" + annotation_name)
             # if annotation_name == "complemgram":
             #     print("----", annotation_name)
             #     m.field("pos_" + annotation_name, Text(analyzer=mappingutil.annotation_analyzer()))
@@ -182,9 +180,6 @@ class CreateIndex:
             else:
                 mapping_type = Keyword()
             m.field("text_" + attr["name"], mapping_type)
-            excludes.append("text_" + attr["name"])
-
-        m.meta("_source", excludes=excludes)
 
         m.field("text_attributes", Object(DisabledObject))
         m.field("dump", Keyword(index=False, doc_values=False))
